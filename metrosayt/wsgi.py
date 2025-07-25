b4d76d3f311db45da65c9e2ecc481c8ad29f5fb7
Sunnat Savriyev <sunnatchiksavriyev@gmail.com>
2025-07-24 23:05:00 -0700

    makemigrate and createsuperuser

--- metrosayt/wsgi.py
+++ metrosayt/wsgi.py
@@ -10,6 +10,11 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('Sunnat', 'sunnatsavriyev39@gmail.com', 'savriyev1123')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'metrosayt.settings')
 
