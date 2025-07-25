67de3431b92f2170183cf62c4335df6da7d224fe
Sunnat Savriyev <sunnatchiksavriyev@gmail.com>
2025-07-24 23:08:33 -0700

    makemigrate and createsuperuser new

--- metrosayt/wsgi.py
+++ metrosayt/wsgi.py
@@ -6,16 +6,16 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
-
 import os
-
 from django.core.wsgi import get_wsgi_application
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('Sunnat', 'sunnatsavriyev39@gmail.com', 'savriyev1123')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'metrosayt.settings')
 
 application = get_wsgi_application()
+
+# Superuser yaratishni bu yerga ko'chirish mumkin (settings yuklanganidan keyin)
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+if not User.objects.filter(username='Sunnat').exists():
+    User.objects.create_superuser('Sunnat', 'sunnatsavriyev39@gmail.com', 'savriyev1123')
