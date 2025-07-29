0a9d09b6b9ab598d1b7ddc809e69b5b53bb003bf
Sunnat Savriyev <sunnatchiksavriyev@gmail.com>
2025-07-29 05:01:56 -0700

    deleted pagenation

--- metrosayt/settings.py
+++ metrosayt/settings.py
@@ -43,8 +43,8 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,  
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 10,  
 }
 
 
