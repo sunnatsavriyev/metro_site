1c583a169b091d4325e38c003d1d401e1bae0eda
Sunnat Savriyev <sunnatchiksavriyev@gmail.com>
2025-08-04 05:40:18 -0700

    add db url

--- metrosayt/settings.py
+++ metrosayt/settings.py
@@ -103,12 +103,8 @@ WSGI_APPLICATION = 'metrosayt.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env("DB_NAME"),
-        'USER': env("DB_USER"),
-        'PASSWORD': env("DB_PASSWORD"),
-        'HOST': env("DB_HOST"),
-        'PORT': env("DB_PORT"),
+         'default': dj_database_url.config(
+            default=os.environ.get('DATABASE_URL'))
     }
 }
 
